55e89da7a2632d1696e90d54cb355f705943cd24
Luddite <dingsf6@gmail.com>
2017-12-30 00:09:00 +0800

    add mouse click class

---
+++ src/mineSweeper/MouseClick.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mineSweeper;
+import java.awt.event.*; //载入MouseAdapter所在的包
+import javax.swing.*; 
+
+/**
+ *
+ * @author Admin
+ */
+public class MouseClick extends MouseAdapter{
+    
+}
