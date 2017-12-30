6a5bc96837bf0df41302b1a680ee233604020d56
Luddite <dingsf6@gmail.com>
2017-12-30 11:01:13 +0800

    add mouseClick

--- src/mineSweeper/MinePanel.java
+++ src/mineSweeper/MinePanel.java
@@ -14,43 +14,60 @@ import java.awt.*;
  */
 public class MinePanel extends JPanel{
     boolean[][] ifOpen;
+    boolean[][] ifBoom;
+    boolean isFirst;
     int column,row;
-    int size;
-    String[][] words;
+    int squareSize;
+    int booms;
+    int order;
+    int[][] count;
     Color black;
     Color white;
     Font myFont;
+    MouseClick mouseClick;
     
-    public void initPanel(int column,int row,int size){
+    public void initPanel(int column,int row,int booms,int size,int order){
         this.column=column;
         this.row=row;
-        this.size=size;
+        this.booms=booms;
+        this.squareSize=size;
+        this.order=order;
         black = new Color(70,70,70);
         white=new Color(240,240,240);
         myFont=new Font("SanSerif",Font.BOLD,size);
+        isFirst=true;
+        mouseClick=new MouseClick();
+        mouseClick.myInit(this);
         ifOpen=new boolean[row][];
-        words=new String[row][];
+        count=new int[row][];
+        ifBoom=new boolean[row][];
         for(int r=0;r<row;++r){
             ifOpen[r]=new boolean[column];
-            words[r]=new String[column];
+            count[r]=new int[column];
+            ifBoom[r]=new boolean[column];
             for(int c=0;c<column;++c){
                 ifOpen[r][c]=false;
-                words[r][c]="";
+                count[r][c]=0;
+                ifBoom[r][c]=false;
             }
         }
     }
     
-    public void paint(Graphics g){
-        super.paint(g);
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
         g.setFont(myFont);
         for(int r=0;r<row;++r){
             for(int c=0;c<column;++c){
                 g.setColor(black);
                 if(ifOpen[r][c]){
-                    g.drawString(words[r][c], c, c);
+                    if(ifBoom[r][c]){
+                        g.drawString("*", squareSize*c, squareSize*r);
+                    }else{
+                        g.drawString(count[r][c]+"", squareSize*c, squareSize*r);
+                    }
                     g.setColor(white);
                 }
-                g.fillRoundRect(size*c, size*r, size-1, size-1, size/2, size/2);
+                g.fillRoundRect(squareSize*c, squareSize*r, squareSize-1, squareSize-1, squareSize/2, squareSize/2);
             }
         }
     }
