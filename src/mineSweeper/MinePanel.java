2aacd2c49bb80328c9c029f574fc8365e63d85df
Luddite <dingsf6@gmail.com>
2017-12-30 17:23:42 +0800

    right click add flag

--- src/mineSweeper/MinePanel.java
+++ src/mineSweeper/MinePanel.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package mineSweeper;
+
 import javax.swing.*;
 import java.lang.*;
 import java.awt.*;
@@ -12,9 +13,12 @@ import java.awt.*;
  *
  * @author Luddite
  */
+
 public class MinePanel extends JPanel{
+    //<editor-fold>
     boolean[][] ifOpen;
     boolean[][] ifBoom;
+    boolean[][] ifFlag;
     boolean isFirst;
     int column,row;
     int squareSize;
@@ -24,6 +28,7 @@ public class MinePanel extends JPanel{
     int[][] count;
     Color black,white,yellow,green;
     Font myFont;
+    //</editor-fold>
     
     public void initPanel(int column,int row,int booms,int size,int order){
         this.column=column;
@@ -36,19 +41,22 @@ public class MinePanel extends JPanel{
         white=new Color(240,240,240);
         yellow=new Color(240,240,100);
         green=new Color(50,240,100);
-        myFont=new Font("SanSerif",Font.BOLD,size-5);
+        myFont=new Font("SanSerif",Font.BOLD,size*2/3);
         isFirst=true;
         ifOpen=new boolean[row][];
         count=new int[row][];
         ifBoom=new boolean[row][];
+        ifFlag=new boolean[row][];
         for(int r=0;r<row;++r){
             ifOpen[r]=new boolean[column];
             count[r]=new int[column];
             ifBoom[r]=new boolean[column];
+            ifFlag[r]=new boolean[column];
             for(int c=0;c<column;++c){
                 ifOpen[r][c]=false;
                 count[r][c]=0;
                 ifBoom[r][c]=false;
+                ifFlag[r][c]=false;
             }
         }
     }
@@ -63,17 +71,20 @@ public class MinePanel extends JPanel{
                     g.fillRoundRect(squareSize*c, squareSize*r, squareSize-1, squareSize-1, squareSize/2, squareSize/2);
                     g.setColor(green);
                     if(ifBoom[r][c]){
-                        g.drawString("*", squareSize*c, squareSize*r+addHeight);
+                        g.drawString(" *", squareSize*c, squareSize*r+addHeight);
                     }else{
                         g.drawString(count[r][c]+"", squareSize*c, squareSize*r+addHeight);
                     }
                 }else{
                     g.setColor(black);
                     g.fillRoundRect(squareSize*c, squareSize*r, squareSize-1, squareSize-1, squareSize/2, squareSize/2);
+                    if(ifFlag[r][c]){
+                        g.setColor(white);
+                        g.drawString(" *", squareSize*c, squareSize*r+addHeight);
+                    }
                 }
             }
         }
-        
     }
     
     
--- src/mineSweeper/MouseClick.java
+++ src/mineSweeper/MouseClick.java
@@ -18,6 +18,7 @@ public class MouseClick extends MouseAdapter{
     Random random;
     Stack<MyPoint> stackPoint;
     int row,column;
+    boolean ifDown;
     
     public void myInit(MinePanel minePanel){
         this.minePanel=minePanel;
@@ -25,6 +26,7 @@ public class MouseClick extends MouseAdapter{
         row=minePanel.row;
         column=minePanel.column;
         stackPoint=new Stack<MyPoint>();
+        
         minePanel.addMouseListener(this);
     }
     
@@ -32,74 +34,85 @@ public class MouseClick extends MouseAdapter{
     public void mouseReleased(MouseEvent e){
         int clickX=e.getX()/minePanel.squareSize;
         int clickY=e.getY()/minePanel.squareSize;
-        if(minePanel.isFirst){
-            minePanel.isFirst=false;
-            int booms=minePanel.booms;
-            int area=row*column-1;
-            int order=minePanel.order;
-            for(int r=0;r<row;++r){
-                for(int c=0;c<column;++c){
-                    if(r!=clickY || c!=clickX){
-                        if(random.nextInt(area)<booms){
-                            minePanel.ifBoom[r][c]=true;
-                            for(int y=r-order;y<=r+order;++y){
-                                for(int x=c-order;x<=c+order;++x){
-                                    if(x>=0&&y>=0&&x<column&&y<row){
-                                        ++minePanel.count[y][x];
+        
+        if(e.getButton()==e.BUTTON1){//<editor-fold>
+            if(minePanel.isFirst){//<editor-fold>
+                minePanel.isFirst=false;
+                int booms=minePanel.booms;
+                int area=row*column-1;
+                int order=minePanel.order;
+                for(int r=0;r<row;++r){
+                    for(int c=0;c<column;++c){
+                        if(r!=clickY || c!=clickX){
+                            if(random.nextInt(area)<booms){
+                                minePanel.ifBoom[r][c]=true;
+                                for(int y=r-order;y<=r+order;++y){
+                                    for(int x=c-order;x<=c+order;++x){
+                                        if(x>=0&&y>=0&&x<column&&y<row){
+                                            ++minePanel.count[y][x];
+                                        }
                                     }
                                 }
+                                --booms;
                             }
-                            --booms;
+                            --area;
                         }
-                        --area;
+                        System.out.print(minePanel.ifBoom[r][c]+"\t");
                     }
-                    System.out.print(minePanel.ifBoom[r][c]+"\t");
+                    System.out.println();
+                }//</editor-fold>
+            }
+            if(minePanel.ifBoom[clickY][clickX]){
+                if(!minePanel.ifFlag[clickY][clickX]){
+                    for(int r=0;r<minePanel.row;++r){
+                        for(int c=0;c<minePanel.column;++c){
+                            if(minePanel.ifBoom[r][c]){
+                                minePanel.ifOpen[r][c]=true;
+                            }
+                        }
+                    }
+                    System.out.println("lose!");
                 }
-                System.out.println();
+            }else{
+                stackPoint.push(new MyPoint(clickX,clickY));
+            }
+            MyPoint thisPoint;
+            int thisx,thisy;
+            while(!stackPoint.empty()){//<editor-fold>
+                thisPoint=stackPoint.pop();
+                thisx=thisPoint.x;
+                thisy=thisPoint.y;
+                if(!minePanel.ifBoom[thisy][thisx]){
+                    minePanel.ifOpen[thisy][thisx]=true;
+                    if(thisy>0){
+                        if(minePanel.ifOpen[thisy-1][thisx]!=true){
+                            stackPoint.push(new MyPoint(thisx,thisy-1));
+                        }
+                    }
+                    if(thisy<row-1){
+                        if(minePanel.ifOpen[thisy+1][thisx]!=true){
+                            stackPoint.push(new MyPoint(thisx,thisy+1));
+                        }
+                    }
+                     if(thisx>0){
+                        if(minePanel.ifOpen[thisy][thisx-1]!=true){
+                            stackPoint.push(new MyPoint(thisx-1,thisy));
+                        }
+                    }
+                    if(thisx<column-1){
+                        if(minePanel.ifOpen[thisy][thisx+1]!=true){
+                            stackPoint.push(new MyPoint(thisx+1,thisy));
+                        }
+                    }
+                }//</editor-fold>
+            }
+            //</editor-fold>
+        }else if(e.getButton()==e.BUTTON3){
+            if(!minePanel.ifOpen[clickY][clickX]){
+                minePanel.ifFlag[clickY][clickX]=!minePanel.ifFlag[clickY][clickX];
             }
         }
-        if(minePanel.ifBoom[clickY][clickX]){
-            for(int r=0;r<minePanel.row;++r){
-                for(int c=0;c<minePanel.column;++c){
-                    if(minePanel.ifBoom[r][c]){
-                        minePanel.ifOpen[r][c]=true;
-                    }
-                }
-            }
-            System.out.println("lose!");
-        }else{
-            stackPoint.push(new MyPoint(clickX,clickY));
-        }
-        MyPoint thisPoint;
-        int thisx,thisy;
-        while(!stackPoint.empty()){
-            thisPoint=stackPoint.pop();
-            thisx=thisPoint.x;
-            thisy=thisPoint.y;
-            if(!minePanel.ifBoom[thisy][thisx]){
-                minePanel.ifOpen[thisy][thisx]=true;
-                if(thisy>0){
-                    if(minePanel.ifOpen[thisy-1][thisx]!=true){
-                        stackPoint.push(new MyPoint(thisx,thisy-1));
-                    }
-                }
-                if(thisy<row-1){
-                    if(minePanel.ifOpen[thisy+1][thisx]!=true){
-                        stackPoint.push(new MyPoint(thisx,thisy+1));
-                    }
-                }
-                 if(thisx>0){
-                    if(minePanel.ifOpen[thisy][thisx-1]!=true){
-                        stackPoint.push(new MyPoint(thisx-1,thisy));
-                    }
-                }
-                if(thisx<column-1){
-                    if(minePanel.ifOpen[thisy][thisx+1]!=true){
-                        stackPoint.push(new MyPoint(thisx+1,thisy));
-                    }
-                }
-            }
-        }
+        
         minePanel.repaint();
         
     }
